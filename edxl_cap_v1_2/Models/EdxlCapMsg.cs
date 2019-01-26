eaec004906f215ef7de8d4093fdf3b7fef8ebe81
Rex Brooks <rexb@starbourne.com>
2019-01-26 11:28:20 -0800

    2nd Milestone Update 01-26-2019

---
+++ edxl_cap_v1_2/Models/EdxlCapMsg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using edxl_cap_v1_2.Models;
+
+namespace edxl_cap_v1_2.Models
+{
+    public class EdxlCapMsg
+    {
+        [Key]
+        public int Id { get; set; }
+        public int AlertIndex { get; set; }
+        public int? SelectedAlertIndex { get; set; }
+        public string Alert_Identifier { get; set; }
+        public int AreaIndex { get; set; }
+        public int? SelectedAreaIndex { get; set; }
+        public int InfoIndex { get; set; }
+        public int? SelectedInfoIndex { get; set; }
+        public int ResourceIndex { get; set; }
+        public int? SelectedResourceIndex { get; set; }
+    }
+}
+
